28bfe6dc5ca706ed7037a7aaaa15f77bf8a538e6
shafiq <makshafiq588@gmail.com>
2022-12-17 07:43:35 +0530

    Hero section finished

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,18 +14,26 @@ const Main = () => {
       setMovies(response.data.results);
     });
   }, []);
-  console.log(movie);
+  // console.log(movie);
+
+  const truncateString = (str , num) => {
+    if(str?.length > num){
+      return str.slice(0, num) + '...' ;
+    }else{
+      return str;
+    }
+  }
   return (
-    <div className="w-full h-[650px]">
+    <div className="w-full h-[600px]">
       <div className="w-full h-full">
-        <div className="w-full h-[650px] absolute bg-gradient-to-r from-stone-800"></div>
+        <div className="w-full h-[600px] absolute bg-gradient-to-r from-stone-800"></div>
         <img
           className="w-full h-full bg-cover object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
       </div>
-      <div className="absolute top-[30%] ml-5 flex flex-col w-1/2">
+      <div className="absolute top-[30%] p-4 md:p-8">
         <h1 className="text-2xl capitalize text-white font-semibold py-3">
           {movie?.title}
         </h1>
@@ -35,11 +43,12 @@ const Main = () => {
             watch later
           </button>
         </div>
-        <span className="text-gray-400 text-xs py-3">
-          Realeased {movie?.release_date}
-        </span>
-        <p className="text-white text-md sm:w-[50%] md:w-[70%] lg:w-[80%] xl:w-[100%]">
-            {movie?.overview}
+        <p className="text-gray-400 text-xs py-3">
+          Realeased: {movie?.release_date}
+        </p>
+        <p  className="text-white text-md w-[80%]
+          md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%]">
+          {truncateString(movie?.overview, 150)}
         </p>
       </div>
     </div>
