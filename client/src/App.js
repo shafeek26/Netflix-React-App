d14f64d9561f9cc81cd248aeb421f85d9a91cbae
shafiq <makshafiq588@gmail.com>
2022-12-18 13:28:01 +0530

    frontend authentication added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import {Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
+import Login from './pages/Login';
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element = {<Home />} />
+        <Route path="/signup" element = {<SignUp />} />
+        <Route path="/login" element = {<Login />} />
       </Routes>
     </div>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,26 @@
-import {FaBars} from 'react-icons/fa'
-// import {Link} from 'react-router-dom';
+/** @format */
+
+import {FaBars} from "react-icons/fa";
+import {Link} from "react-router-dom";
 const Navbar = () => {
     return (
-        <div className='w-full flex items-center justify-between h-24  p-5 absolute z-[100]'>
-            <h1 className='text-red-600 text-4xl font-bold uppercase'>Netflix</h1>
-            <div className='space-x-3 hidden md:block'>
-                <button className='text-white border border-white py-1 px-4'>Sign Up</button>
-                <button className='text-white bg-red-600 py-1 px-6'>Login</button>
+        <div className="w-full flex items-center justify-between h-24  p-5 absolute z-[100]">
+            <Link to="/">
+                <h1 className="text-red-600 text-4xl font-bold uppercase">Netflix</h1>
+            </Link>
+            <div className="space-x-3 hidden md:block">
+                <Link to='/signup'>
+                    <button className="text-white border border-white py-1 px-4">
+                        Sign Up
+                    </button>
+                </Link>
+                <Link to='/login'>
+                    <button className="text-white bg-red-600 py-1 px-6">Login</button>
+                </Link>
             </div>
-            <FaBars className='text-white text-3xl block md:hidden'/>
+            <FaBars className="text-white text-3xl block md:hidden"/>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
